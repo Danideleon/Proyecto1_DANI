e36dfb0e8edfdb791b8b64bf37a252da300f78ef
Danideleon <108084911+Danideleon@users.noreply.github.com>
2023-10-17 22:34:24 -0400

    Implementacion de backlogManager

--- src/edu/ncsu/csc216/product_backlog/model/backlog/BacklogManager.java
+++ src/edu/ncsu/csc216/product_backlog/model/backlog/BacklogManager.java
@@ -3,7 +3,13 @@
  */
 package edu.ncsu.csc216.product_backlog.model.backlog;
 
+import java.io.IOException;
+import java.util.List;
+
 import edu.ncsu.csc216.product_backlog.model.command.Command;
+import edu.ncsu.csc216.product_backlog.model.io.ProductsReader;
+import edu.ncsu.csc216.product_backlog.model.io.ProductsWriter;
+import edu.ncsu.csc216.product_backlog.model.product.Product;
 import edu.ncsu.csc216.product_backlog.model.task.Task;
 import edu.ncsu.csc216.product_backlog.model.task.Task.Type;
 
@@ -12,6 +18,14 @@ import edu.ncsu.csc216.product_backlog.model.task.Task.Type;
  * or current Product,and handles events from the GUI.
  */
 public class BacklogManager {
+	
+	/**An instance of itself**/
+	private static BacklogManager instance;
+	/**List of products**/
+	private List<Product>products;
+	/**The current product being used**/
+	private Product currentProduct;
+	
 	
 	/**
 	 * Constructor of the BacklogManager
@@ -25,23 +39,40 @@ public class BacklogManager {
 	 * @return The instance of BacklogManager
 	 */
 	public static BacklogManager getInstance() {
-		return null;	
+		if (instance == null) {
+			instance = new BacklogManager();
+		}
+		return instance;	
 	}
 	
 	/**
 	 * WriteS the Products to the file using the ProductsWriter class.
 	 * @param filename The name of the file to be written on.
 	 */
-	public void saveToFile(String filename) {
-		// TODO Auto-generated method stub
+	public void saveToFile(String filename){
+		if(currentProduct == null || currentProduct.getTasks().isEmpty()) {
+			throw new IllegalArgumentException("Unable to save file.");
+		}
+		
+		try {
+			ProductsWriter.writeProductsToFile(filename, products);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	/**
 	 * Uses the ProductsReader to read the given fileName
 	 * @param filename The name of the file to be read from
 	 */
-	public void loadFromFile(String filename) {
-		// TODO Auto-generated method stub
+	public void loadFromFile(String filename){
+		
+		List<Product> productsFromFile = ProductsReader.readProductsFile(filename);
+		
+		if(!productsFromFile.isEmpty()) {
+			products.addAll(productsFromFile);
+			currentProduct = productsFromFile.get(0);
+		}
 	}
 	
 	/**
@@ -49,7 +80,14 @@ public class BacklogManager {
 	 * @param productName The name of the product to be used. 
 	 */
 	public void loadProduct(String productName) {
-		// TODO Auto-generated method stub
+		for(Product product : products) {
+			if(product.getProductName().equalsIgnoreCase(productName)) {
+			currentProduct = product;
+			return;
+			}
+		}
+		
+		throw new IllegalArgumentException("Product not available");
 	}
 	
 	/**
@@ -57,7 +95,7 @@ public class BacklogManager {
 	 * @param product The product to be checked
 	 */
 	private void isDuplicateProduct(String product) {
-		// TODO Auto-generated method stub
+		
 	}
 	
 	/**
@@ -65,7 +103,22 @@ public class BacklogManager {
 	 * @return 2D String array that is used to populate the TaskTableModel
 	 */
 	public String[][] getTasksAsArray(){
-		return null;
+		if(currentProduct == null) {
+			return new String[0][0];
+		}
+		
+		List<Task> tasks = currentProduct.getTasks();
+		String [][] taskArray = new String [tasks.size()][4];
+		
+		for (int i = 0; i < tasks.size(); i++) {
+			Task task = tasks.get(i);
+			taskArray[i][0] = Integer.toString(task.getTaskId());
+			taskArray[i][1] = task.getStateName();
+			taskArray[i][2] = task.getTypeLongName();
+			taskArray[i][3] = task.getTitle();
+		}
+		
+		return taskArray;
 	}
 	
 	/**
@@ -74,6 +127,17 @@ public class BacklogManager {
 	 * @return The task with the given ID
 	 */
 	public Task getTaskById(int taskId) {
+		if(currentProduct == null) {
+			return null;
+		}
+		
+		List<Task> tasks = currentProduct.getTasks();
+		for(Task task : tasks) {
+			if(task.getTaskId() == taskId) {
+				return task;
+			}
+		}
+		
 		return null;
 	}
 	
@@ -83,7 +147,17 @@ public class BacklogManager {
 	 * @param c The command to be executed 
 	 */
 	public void executeCommand(int taskId, Command c) {
-		// TODO Auto-generated method stub
+		if(currentProduct == null) {
+			return;
+		}
+		
+		List<Task> tasks = currentProduct.getTasks();
+		for(Task task : tasks) {
+			if(task.getTaskId() == taskId) {
+				executeCommand(taskId, c);
+				return;
+			}
+		}
 	}
 	
 	/**
@@ -91,7 +165,17 @@ public class BacklogManager {
 	 * @param taskId The ID of the task to be deleted
 	 */
 	public void deleteTaskById(int taskId) {
-		// TODO Auto-generated method stub
+		if(currentProduct == null) {
+			return;
+		}
+		
+		List<Task> tasks = currentProduct.getTasks();
+		for(Task task : tasks) {
+			if(task.getTaskId() == taskId) {
+				tasks.remove(taskId);
+				return;
+			}
+		}
 	}
 	
 	/**
@@ -102,7 +186,9 @@ public class BacklogManager {
 	 * @param owner The owner of the task
 	 */
 	public void addTaskToProduct(String title, Type type, String creator, String owner) {
-		// TODO Auto-generated method stub
+		if(currentProduct == null) {
+			return;
+		}
 	}
 	
 	/**
@@ -110,6 +196,10 @@ public class BacklogManager {
 	 * @return The product name for the Current Product
 	 */
 	public String getProductName() {
+		if(currentProduct != null) {
+			return currentProduct.getProductName();
+		}
+		
 		return null;
 	}
 	
@@ -118,14 +208,21 @@ public class BacklogManager {
 	 * @return An array with the List of Products 
 	 */
 	public String[] getProductList() {
-		return null;
+		String[] productList = new String [products.size()];
+		
+		for(int i = 0; i < products.size(); i++) {
+			productList[i] = products.get(i).getProductName();
+		}
+		
+		return productList;
 	}
 	
 	/**
 	 * Resets products to an empty list
 	 */
 	public void clearProducts() {
-		// TODO Auto-generated method stub
+		products.clear();
+		currentProduct = null;
 	}
 	
 	/**
@@ -133,7 +230,23 @@ public class BacklogManager {
 	 * @param updateName The updated name of the Product
 	 */
 	public void editProduct(String updateName) {
-		// TODO Auto-generated method stub
+		if(currentProduct == null) {
+			throw new IllegalArgumentException ("No product selected");
+		}
+		
+		if(updateName == null || updateName.isEmpty()) {
+			throw new IllegalArgumentException("Invalid Product name.");
+		}
+		
+		String updateNameLower = updateName.toLowerCase();
+		
+		for (Product product : products) {
+			if(product != currentProduct && product.getProductName().toLowerCase().equals(updateNameLower)) {
+			throw new IllegalArgumentException("Product name already exists.");
+			}
+		}
+		
+		currentProduct.setProductName(updateName);
 	}
 	
 	/**
@@ -141,13 +254,37 @@ public class BacklogManager {
 	 * @param productName The name of the Product to be added to the list.
 	 */
 	public void addProduct(String productName) {
-		// TODO Auto-generated method stub
+		if(productName == null || productName.isEmpty()) {
+			throw new IllegalArgumentException("Invalid product name.");
+		}
+		
+		String productNameLower = productName.toLowerCase();
+		
+		for (Product product : products) {
+			if (product.getProductName().toLowerCase().equals(productNameLower)) {
+				throw new IllegalArgumentException("Invalid product name.");
+			}
+		}
+		
+		Product newProduct = new Product(productName);
+		products.add(newProduct);
+		loadProduct(productName);
 	}
 	/**
 	 * Deletes the currentProduct
 	 */
 	public void deleteProduct() {
-		// TODO Auto-generated method stub
+		if(currentProduct == null) {
+			throw new IllegalArgumentException("No product selected");
+		}
+		
+		products.remove(currentProduct);
+		
+		if(products.isEmpty()) {
+			currentProduct = null;
+		} else {
+			currentProduct = products.get(0);
+		}
 	}
 	
 	/**
